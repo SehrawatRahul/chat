26799179e5091018134c72c83d6356548b489ac9
SehrawatRahul <90616206+SehrawatRahul@users.noreply.github.com>
2022-02-20 22:42:03 +0530

    Update client.js

--- js/client.js
+++ js/client.js
@@ -4,13 +4,17 @@ const form=document.getElementById('send-container');
 const messageInput=document.getElementById('messageInput');
 const messageContainer=document.querySelector(".container")
 
+var audio = new Audio('ting.mp3');
+
 const append =(message,position)=>{
     const messageElement=document.createElement('div');
     messageElement.innerText=message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
-
+     if(position =='left'){
+        audio.play();
+    }
 }
 const name1 = prompt("Enter Your name to join");
 socket.emit('new-user-joined',name1);
